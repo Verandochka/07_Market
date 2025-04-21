69478b9c1b5c36adac3cf8a0a5bf3d3ecd936ef0
Verandochka <24a_pyeyu@liceum.ztu.edu.ua>
2025-04-21 09:51:26 +0300

    Додано інформацію у script

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 console.log('Перевірка підключення файлу скриптів market.js')
 
+let itemsArray = [
+    'Мотокоса',
+    'Тример',
+    'Газонокосарка',
+    'Оприскувач']
+
 let itemsDiv = document.getElementById("items");
 
 if (itemsDiv) {
-    console.log(itemsDiv)
-    //Додавання відформатованого HTML коду в блок
-   itemsDiv.innerHTML +='<div class = "item"></div>'
-   itemsDiv.innerHTML +='<div class = "item"></div>'
-   itemsDiv.innerHTML +='<div class = "item"></div>'
-   itemsDiv.innerHTML +='<div class = "item"></div>'
-   for (let i=0; i < 100; i++) {
-    itemsDiv.innerHTML +='<div class = "item"></div>'
-   }
+   itemsArray.forEach((item) => {
+    itemsDiv.innerHTML += 
+    `
+    <div class = "item">
+    <h2>Товар №${index}</h2>
+    <p>${item}<p>
+    </div>
+    `
+   })
 } else {
     console.log('Блок товарів не знайдено')
 }
