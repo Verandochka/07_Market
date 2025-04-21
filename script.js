5ef17dc771108fc66f9d1d0623bdbd03f6b98ada
Verandochka <24a_pyeyu@liceum.ztu.edu.ua>
2025-04-21 09:25:41 +0300

    Створено та оформлено початкову структуру SCRIPT

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Перевірка підключення файлу скриптів market.js')
+
+let itemsDiv = document.getElementById("items");
+
+if (itemsDiv) {
+    console.log(itemsDiv)
+    //Додавання відформатованого HTML коду в блок
+   itemsDiv.innerHTML +='<div class = "item"></div>'
+   itemsDiv.innerHTML +='<div class = "item"></div>'
+   itemsDiv.innerHTML +='<div class = "item"></div>'
+   itemsDiv.innerHTML +='<div class = "item"></div>'
+   for (let i=0; i < 100; i++) {
+    itemsDiv.innerHTML +='<div class = "item"></div>'
+   }
+} else {
+    console.log('Блок товарів не знайдено')
+}
